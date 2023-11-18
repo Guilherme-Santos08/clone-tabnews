5b264e0a0cf63332380fccd263bd3010e778ceba
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2023-11-18 15:12:09 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request, response) {
-  response.status(200).json({ message: "DzScript é acima da média" });
+import database from '../../../../infra/database.js'
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;')
+  console.log(result.rows[0].sum)
+
+  response.status(200).json({ message: 'DzScript é acima da média' })
 }
 
-export default status;
+export default status
