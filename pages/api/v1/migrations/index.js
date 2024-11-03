463c1d5f655efc52e8ebd8b7c7d883fa584dff06
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2024-11-03 18:50:23 -0300

    fix(migrations): resolve open connections issue

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,38 +3,49 @@ import { join } from 'node:path'
 import database from 'infra/database'
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient()
-
-  const defaultMigrationOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join('infra', 'migrations'),
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pgmigrations',
+  const requestAllowedMethods = ['GET', 'POST']
+  if (!requestAllowedMethods.includes(request.method)) {
+    return response
+      .status(405)
+      .json({ error: `Method ${request.method} not allowed` })
   }
 
-  if (request.method === 'GET') {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions)
-    await dbClient.end()
+  let dbClient
 
-    response.status(200).json(pendingMigrations)
-  }
+  try {
+    dbClient = await database.getNewClient()
 
-  if (request.method === 'POST') {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    })
-
-    await dbClient.end()
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations)
+    const defaultMigrationOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join('infra', 'migrations'),
+      direction: 'up',
+      verbose: true,
+      migrationsTable: 'pgmigrations',
     }
 
-    response.status(200).json(migratedMigrations)
-  }
+    if (request.method === 'GET') {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions)
 
-  return response.status(405).end()
+      response.status(200).json(pendingMigrations)
+    }
+
+    if (request.method === 'POST') {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      })
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations)
+      }
+
+      response.status(200).json(migratedMigrations)
+    }
+  } catch (err) {
+    console.error(err)
+    return response.status(500).json({ error: 'Internal server error' })
+  } finally {
+    await dbClient.end()
+  }
 }
